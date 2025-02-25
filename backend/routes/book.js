7f229bc35bc4c39cb94c93c6dc4513d2f492a617
Vincent Fuseau <vincent.fuseau@hotmail.fr>
2025-02-25 16:03:08 +0100

    Création et configuration du middleware d'authentification avec la méthode verify de JWT. Ajout de auth aux routes.

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const router = express.Router();
 
 const bookCtrl = require('../controllers/book');
 
 router.get('/:id', bookCtrl.getOneBook);
 router.get('/', bookCtrl.getAllBooks);
-router.post('/', bookCtrl.createBook);
-router.put('/:id', bookCtrl.updateBook);
-router.delete('/:id', bookCtrl.deleteBook);
+router.post('/', auth, bookCtrl.createBook);
+router.put('/:id', auth, bookCtrl.updateBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
 
 module.exports = router;
