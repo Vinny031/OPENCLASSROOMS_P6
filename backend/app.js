eaa47b90fe5253fe5d03dbf65b89599d87927f49
Vincent Fuseau <vincent.fuseau@hotmail.fr>
2025-02-25 15:01:04 +0100

    Configuration des controllers et optimisation de la structure backend.

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require("dotenv").config({ path: ".env" });
 
 const app = express();
-const Book = require('./models/book');
+const bookRoutes = require('./routes/book');
 
 app.use(express.json());
 
@@ -14,40 +14,7 @@ app.use(cors({
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content', 'Accept', 'Content-Type', 'Authorization']
 }));
 
-app.post('/api/book', (req, res, next) => {
-    delete req.body._id;
-    const book = new Book({
-        ...req.body,
-    });
-
-    book.save()
-        .then(() => res.status(201).json({ message: 'Livre ajouté avec succès' }))
-        .catch(error => res.status(400).json({ error: error.message }));
-});
-
-app.get('/api/book', (req, res, next) => {
-    Book.find()
-        .then(books => res.status(200).json(books))
-        .catch(error => res.status(400).json({ error: error.message }));
-  });
-
-app.get('/api/book/:id', (req, res, next) => {
-    Book.findOne({ _id: req.params.id })
-        .then(book => res.status(200).json(book))
-        .catch(error => res.status(404).json({ error }));
-});
-
-app.put('/api/book/:id', (req, res, next) => {
-    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Livre modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-  app.delete('/api/book/:id', (req, res, next) => {
-    Book.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Livre supprimé !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
+app.use('/api/book', bookRoutes);
 
 mongoose.connect(process.env.CONNECTION_STRING)
     .then(() => console.log('✅ Connexion à MongoDB réussie !'))
